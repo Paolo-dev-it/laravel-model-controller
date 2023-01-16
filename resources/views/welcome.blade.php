9723a1def839082c5d6f4e986997c5339c4bf369
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-16 14:55:25 +0100

    Created card. Project completed!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,37 +4,27 @@
 
 @section('main-content')
 
-    {{-- @foreach ($movies_all as $elem)
+    <div class="bg-secondary">
         <div class="container">
-            <div class="card-cont">
-                <h2>{{ $elem->title }}</h2>
-                <h3>Titolo originale: {{ $elem->original_title }}</h3>
-                <h3>Lingua: {{ $elem->nationality }}</h3>
-                <h4>Data di rilascio: {{ $elem->date }}</h4>
-                <h4>Voto: {{ $elem->vote }}</h4>
-            </div>
-
-        </div>
-    @endforeach --}}
-
-
-    <div class="container">
-        <div class="row">
-            @foreach ($movies_all as $elem)
-                <div class="col-4">
-                    <div class="card h-100" style="width: 18rem;">
-                        <div class="card-body">
-                            <h2 class="card-title">{{ $elem->title }}</h2>
-                            <h3>{{ $elem->original_title }}</h3>
-                            <h5>Lingua: {{ $elem->nationality }}</h5>
-                            <h5>Data di rilascio: {{ $elem->date }}</h5>
-                            <h5>Voto: {{ $elem->vote }}</h5>
+            <div class="row">
+                <h1 class="text-center text-warning">Movies</h1>
+                @foreach ($movies_all as $elem)
+                    <div class="col-4 d-flex text-center justify-content-center mt-3 p-3">
+                        <div class="card h-100" style="width: 18rem;">
+                            <div class="card-body">
+                                <h2 class="card-title">{{ $elem->title }}</h2>
+                                <h3>{{ $elem->original_title }}</h3>
+                                <h5>Lingua: {{ $elem->nationality }}</h5>
+                                <h5>Data di rilascio: {{ $elem->date }}</h5>
+                                <h5>Voto: {{ $elem->vote }}</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
+
 
 
 
