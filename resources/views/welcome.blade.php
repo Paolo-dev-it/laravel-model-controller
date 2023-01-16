f224ea6e604a2ade8e29f2c78dba4f31925a81e8
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-16 14:40:18 +0100

    Added bootstrap. Project completed!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,39 @@
 
 @section('main-content')
 
-    @foreach ($movies_all as $elem)
+    {{-- @foreach ($movies_all as $elem)
         <div class="container">
-            <h2>{{ $elem->title }}</h2>
-            <h3>{{ $elem->original_title }}</h3>
-            <h3>{{ $elem->nationality }}</h3>
-            <h4>{{ $elem->date }}</h4>
-            <h4>{{ $elem->vote }}</h4>
+            <div class="card-cont">
+                <h2>{{ $elem->title }}</h2>
+                <h3>Titolo originale: {{ $elem->original_title }}</h3>
+                <h3>Lingua: {{ $elem->nationality }}</h3>
+                <h4>Data di rilascio: {{ $elem->date }}</h4>
+                <h4>Voto: {{ $elem->vote }}</h4>
+            </div>
+
         </div>
-    @endforeach
+    @endforeach --}}
+
+
+    <div class="container">
+        <div class="row">
+            @foreach ($movies_all as $elem)
+                <div class="col-4">
+                    <div class="card h-100" style="width: 18rem;">
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $elem->title }}</h2>
+                            <h3>{{ $elem->original_title }}</h3>
+                            <h5>Lingua: {{ $elem->nationality }}</h5>
+                            <h5>Data di rilascio: {{ $elem->date }}</h5>
+                            <h5>Voto: {{ $elem->vote }}</h5>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+
+
 
 @endsection
